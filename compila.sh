3ce1676a65f814273c683d8b0ac79b893a5c4173
Prando <gprando55@gmail.com>
2019-12-26 18:32:04 -0300

    add script para compilar arquivo c

---
+++ compila.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#########################################################################
+# 									                                    #
+# Compila.sh - compilar e executar arquivo em C 			            #
+#									                                    #
+# Autor: Gabriel Prando (gprando55@gmail.com)                          	#
+# Data Criação: 26/12/2019				                        		#
+#									                                    #
+# Descrição: Script para compilar arquivo C e execuar                 	#
+#	     passando o nome do arquivo .c como parêmetro	                #
+#	                                                                	#
+#									                                    #
+# Exemplo de uso: ./compila.sh	arquivo.c	                    		#
+#									                                    #
+#########################################################################
+
+clear
+
+gcc $1 -o teste
+./teste
+echo ""
---
+++ compilaTAD.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#########################################################################
+# 									                                    #
+# Compila.sh - compilar TAD e executar           			            #
+#									                                    #
+# Autor: Gabriel Prando (gprando55@gmail.com)                          	#
+# Data Criação: 26/12/2019				                        		#
+#									                                    #
+# Descrição: Script para compilar TAD em C e execuar                 	#
+#	     passando o nome do arquivo das funções e o usa_arquivo .c      #
+#                       como parêmetro	                                #
+#	                                                                	#
+#									                                    #
+# Exemplo de uso: ./compila.sh	arquivo1.c	usa_arquivo.c          		#
+#									                                    #
+#########################################################################
+
+clear
+
+gcc $1 $2 -o teste
+./teste
+echo ""

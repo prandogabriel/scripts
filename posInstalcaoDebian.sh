e1831b7d3a78c4697710f19d7c94352692211199
Prando <gprando55@gmail.com>
2019-12-26 19:26:53 -0300

    ADD

---
+++ posInstalcaoDebian.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#########################################################################
+# 									                                    
+# posInstalacaoDebian.sh - script atomatização de instalação pós instalar SO           			            
+#									                                    
+# Autor: Gabriel Prando (gprando55@gmail.com)                          	
+# Data Criação: xx/xx/YYYY				                        		
+#									                                    
+# Descrição: instalar todos os programas e softwares necessários no Debian                 	
+#	     pós formatação	                                
+#	                                                                										                                    #
+# Exemplo de uso: sudo ./posInstalacaoDebian.sh	         		
+#							                                    
+#########################################################################
+
+######################################## 
+#
+#       EM BREVE
+#
+#######################################

cfea9bb522a4a44abd277de5b4163a52aaa289ba
Prando <gprando55@gmail.com>
2019-12-26 18:36:16 -0300

    add script para compilar arquivo c

--- compila.sh
+++ compila.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #########################################################################
-# 									                                    #
-# Compila.sh - compilar e executar arquivo em C 			            #
-#									                                    #
-# Autor: Gabriel Prando (gprando55@gmail.com)                          	#
-# Data Criação: 26/12/2019				                        		#
-#									                                    #
-# Descrição: Script para compilar arquivo C e execuar                 	#
-#	     passando o nome do arquivo .c como parêmetro	                #
-#	                                                                	#
-#									                                    #
-# Exemplo de uso: ./compila.sh	arquivo.c	                    		#
-#									                                    #
+# 									                                    
+# Compila.sh - compilar e executar arquivo em C 			            
+#									                                    
+# Autor: Gabriel Prando (gprando55@gmail.com)                          	
+# Data Criação: 26/12/2019				                        		
+#									                                    
+# Descrição: Script para compilar arquivo C e execuar                 	
+#	     passando o nome do arquivo .c como parêmetro	                
+#	                                                                	
+#									                                    
+# Exemplo de uso: ./compila.sh	arquivo.c	                    		
+#									                                    
 #########################################################################
 
 clear
--- compilaTAD.sh
+++ compilaTAD.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 #########################################################################
-# 									                                    #
-# Compila.sh - compilar TAD e executar           			            #
-#									                                    #
-# Autor: Gabriel Prando (gprando55@gmail.com)                          	#
-# Data Criação: 26/12/2019				                        		#
-#									                                    #
-# Descrição: Script para compilar TAD em C e execuar                 	#
-#	     passando o nome do arquivo das funções e o usa_arquivo .c      #
-#                       como parêmetro	                                #
-#	                                                                	#
-#									                                    #
-# Exemplo de uso: ./compila.sh	arquivo1.c	usa_arquivo.c          		#
-#									                                    #
+# 									                                    
+# Compila.sh - compilar TAD e executar           			            
+#									                                    
+# Autor: Gabriel Prando (gprando55@gmail.com)                          	
+# Data Criação: 26/12/2019				                        		
+#									                                    
+# Descrição: Script para compilar TAD em C e execuar                 	
+#	     passando o nome do arquivo das funções e o usa_arquivo .c      
+#                       como parêmetro	                                
+#	                                                                										                                    #
+# Exemplo de uso: ./compila.sh	arquivo1.c	usa_arquivo.c          		
+#							                                    
 #########################################################################
 
 clear
